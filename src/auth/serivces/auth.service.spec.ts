3cbd6d53e97c20e2aef2ce1d5ee9688f8fcba7a8
InakiGT <yarimfuego@gmail.com>
2024-03-11 08:40:11 -0600

    [ADDED] GenerateJwt Method

--- src/auth/serivces/auth.service.spec.ts
+++ src/auth/serivces/auth.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+
 import { AuthService } from './auth.service';
+import { UsersService } from '../../users/services/users/users.service';
+import { User } from '../../users/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        UsersService,
+        JwtService,
+        { provide: getModelToken(User.name), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
--- src/auth/serivces/auth.service.ts
+++ src/auth/serivces/auth.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from '../../users/services/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+
+import { UsersService } from '../../users/services/users/users.service';
+import { User } from '../../users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async ValidateUser(email: string, password: string) {
     const user = await this.usersService.FindOne({ email });
@@ -21,5 +27,13 @@ export class AuthService {
     }
 
     return null;
+  }
+
+  GenerateJwt(user: User) {
+    const payload = { sub: user.id };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
